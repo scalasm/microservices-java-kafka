4779dc87377e406a934ecd063ad193a996143b4e
Mario Scalas <mario.scalas@gmail.com>
2022-07-07 15:32:48 +0000

    Added @Payload annotations for each implemented method.

--- account/account-query/src/main/java/com/techbank/account/query/infrastructure/consumers/AccountEventConsumer.java
+++ account/account-query/src/main/java/com/techbank/account/query/infrastructure/consumers/AccountEventConsumer.java
@@ -2,6 +2,7 @@ package com.techbank.account.query.infrastructure.consumers;
 
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 import com.techbank.account.common.events.AccountClosedEvent;
@@ -19,28 +20,28 @@ public class AccountEventConsumer implements EventConsumer {
     
     @KafkaListener(topics = "AccountOpenedEvent", groupId = "${spring.kafka.consumer.group-id}")
     @Override
-    public void consume(final AccountOpenedEvent event, final Acknowledgment ack) {
+    public void consume(final @Payload AccountOpenedEvent event, final Acknowledgment ack) {
         eventHandler.on(event);
         ack.acknowledge();
     }
 
     @KafkaListener(topics = "AccountClosedEvent", groupId = "${spring.kafka.consumer.group-id}")
     @Override
-    public void consume(final AccountClosedEvent event, final Acknowledgment ack) {
+    public void consume(final @Payload AccountClosedEvent event, final Acknowledgment ack) {
         eventHandler.on(event);
         ack.acknowledge();
     }
 
     @KafkaListener(topics = "FundsDepositedEvent", groupId = "${spring.kafka.consumer.group-id}")
     @Override
-    public void consume(final FundsDepositedEvent event, final Acknowledgment ack) {
+    public void consume(final @Payload FundsDepositedEvent event, final Acknowledgment ack) {
         eventHandler.on(event);
         ack.acknowledge();
     }
 
     @KafkaListener(topics = "FundsWithdrawnEvent", groupId = "${spring.kafka.consumer.group-id}")
     @Override
-    public void consume(final FundsWithdrawnEvent event, final Acknowledgment ack) {
+    public void consume(final @Payload FundsWithdrawnEvent event, final Acknowledgment ack) {
         eventHandler.on(event);
         ack.acknowledge();
     }
